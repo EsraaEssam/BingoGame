8bac186dc727a1bf26f92d7e90175551acd4d8b8
EsraaEssam <Esraa_em95@outlook.com>
2020-10-03 23:37:24 +0200

    Update fb.js
    Remove facebook API key & secret

--- configuration/fb.js
+++ configuration/fb.js
@@ -4,8 +4,8 @@ module.exports = {
 * It consists of Twitter app information, database information.
 */
 
-	"facebook_api_key" 			: 	"1550411981921703",
-	"facebook_api_secret"			:	"2a8fee1a0622fe6b852b8c454fe9406e",
+	"facebook_api_key" 			: 	"Facebook_api_key",
+	"facebook_api_secret"			:	"facebook_api_secret",
 	"callback_url"				:	"http://cmpassign3.cloudapp.net:22445/auth/facebook/callback",
 	"use_database"				:	"false",
 	"host"					:	"http://cmpassign3.cloudapp.net",
