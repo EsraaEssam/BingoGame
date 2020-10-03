535f0a62d05d127cb0ae77e2673a162c891be627
EsraaEssam <Esraa_em95@outlook.com>
2020-10-03 23:39:30 +0200

    Update twitter.js
    Remove twitter API key & sevret

--- configuration/twitter.js
+++ configuration/twitter.js
@@ -4,8 +4,8 @@ module.exports={
 * It consists of Twitter app information, database information.
 */
 
-	"twitter_api_key" 		: 		"evfdbGHQ3wemYbvya56tIFnZj",
-	"twitter_api_secret"		:		"MJQWjukkdqC3RmNmJk1zE1Y1YMuVmAZyNL8R9X1inLwdaTvk4r",
+	"twitter_api_key" 		: 		"twitter_api_key",
+	"twitter_api_secret"		:		"twitter_api_secret",
 	"callback_url"			:		"http://cmpassign3.cloudapp.net:22445/auth/twitter/callback",
 	"use_database"			:		"false",
 	"host"				:		"http://cmpassign3.cloudapp.net"
